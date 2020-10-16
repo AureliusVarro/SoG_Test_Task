b80b55a77154b52dc309082ae8c8e8efe0253cff
Denis P <megadensis@gmail.com>
2020-10-16 22:15:19 +0300

    Added, Fixed
    Added instigate enemy on effects
    Added in CharacterEnemyBase source ASC Instigaror, source ASC death
    
    FIxed attack enemy, death enemy, kill points

--- Source/TestProject/Private/TP_EnemyCharacterBase.cpp
+++ Source/TestProject/Private/TP_EnemyCharacterBase.cpp
@@ -2,14 +2,20 @@
 
 
 #include "TP_EnemyCharacterBase.h"
+#include "GameplayEffect.h"
+#include "GameplayEffectExtension.h"
+//#include "Net/UnrealNetwork.h"
+#include "GameplayTagContainer.h"
 #include "..\Public\TP_EnemyCharacterBase.h"
 
-// Sets default values
+
 ATP_EnemyCharacterBase::ATP_EnemyCharacterBase()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	bIsDead = false;
+	//bIsDead = false;
+
+	DeadTag = FGameplayTag::RequestGameplayTag(FName("State.Dead"));
 	
 }
 
@@ -24,31 +30,74 @@ void ATP_EnemyCharacterBase::BeginPlay()
 
 	if (AbilitySystemComponent)//TODO: error
 	{
+	
+		// Called on server whenever a GE is applied to self. This includes instant and duration based GEs.
+		AbilitySystemComponent->OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &ATP_EnemyCharacterBase::OnGameplayEffectApplied);
 		// Attribute change callbacks
 		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AttributeSet->GetHealthAttribute()).AddUObject(this, &ATP_EnemyCharacterBase::HealthChanged);
 	}
-	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("World delta for current frame equals %f"), AttributeSet->Health.GetBaseValue()));
+	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("GetBaseValue %f"), AttributeSet->Health.GetBaseValue()));
 	
 	
 }
 
-void ATP_EnemyCharacterBase::Death_Implementation()
+void ATP_EnemyCharacterBase::OnGameplayEffectApplied(UAbilitySystemComponent * Source, const FGameplayEffectSpec & Spec, FActiveGameplayEffectHandle Handle)
 {
-	if(!bIsDead)
+	//Instigate only player
+	bool IsPlayer = Source->GetAvatarActor()->GetInstigatorController()->IsPlayerController();
+
+	if (!IsPlayer)
 	{
-		OnEnemyDeathDelegate.Broadcast();
-		bIsDead = true;
+		return;
 	}
-	
-	
+
+	FGameplayTagContainer ContainTags;
+	Spec.GetAllAssetTags(ContainTags);
+
+	if (ContainTags.HasAny(InstigateTags) && !ASCSourceInstigator)
+	{
+		ASCSourceInstigator = Source;
+		OnInstigateDelegate.Broadcast(ASCSourceInstigator);
+	}
+
+}
+
+
+//void ATP_EnemyCharacterBase::Death_Implementation()
+//{
+//
+//}
+
+void ATP_EnemyCharacterBase::Death()
+{
+	UE_LOG(LogTemp, Warning, TEXT("dddddddddddddddddddddd"));
+	OnEnemyDeathDelegate.Broadcast();
+	//ASCSourceInstigate = nullptr;
+	AbilitySystemComponent->OnGameplayEffectAppliedDelegateToSelf.RemoveAll(this);
 }
 
 void ATP_EnemyCharacterBase::HealthChanged(const FOnAttributeChangeData & Data)
 {
+	
+	if (!IsAlive() && !AbilitySystemComponent->HasMatchingGameplayTag(DeadTag))
+	{
+		
+		//damage from the effect
+		if (Data.GEModData)
+		{
+			FGameplayEffectContextHandle Context = Data.GEModData->EffectSpec.GetContext();
+			UAbilitySystemComponent* Source = Context.GetOriginalInstigatorAbilitySystemComponent();
+			UAbilitySystemComponent* SourceAbilitySystemComponent = Context.GetOriginalInstigatorAbilitySystemComponent();
+			//AActor* SourceActor = SourceAbilitySystemComponent ? SourceAbilitySystemComponent->AvatarActor : nullptr;
+			if (SourceAbilitySystemComponent)
+			{
+				ASCSourceDeath = SourceAbilitySystemComponent;
+			}
 
-	if (Data.NewValue <= 0)
+		}
+		
 		Death();
-
+	}
 }
 // Called every frame
 void ATP_EnemyCharacterBase::Tick(float DeltaTime)
@@ -63,3 +112,37 @@ void ATP_EnemyCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerIn
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
+
+bool ATP_EnemyCharacterBase::IsAlive() const
+{
+	return AttributeSet->GetHealth() > 0.0f;
+}
+
+UAbilitySystemComponent * ATP_EnemyCharacterBase::GetASCSourceInstigator()
+{
+	return ASCSourceInstigator;
+	
+}
+
+UAbilitySystemComponent * ATP_EnemyCharacterBase::GetASCSourceDeath()
+{
+	return ASCSourceDeath;
+
+}
+
+void ATP_EnemyCharacterBase::SetASCSourceInstigator(UAbilitySystemComponent * value)
+{
+	ASCSourceInstigator = value;
+}
+
+
+
+int ATP_EnemyCharacterBase::GetPointsPerKill()
+{
+	return PointsPerKill;
+}
+
+//int ATP_EnemyCharacterBase::GetBaseDamage()
+//{
+//	return BaseDamage;
+//}
--- Source/TestProject/Public/TP_EnemyCharacterBase.h
+++ Source/TestProject/Public/TP_EnemyCharacterBase.h
@@ -10,10 +10,11 @@
 #include "AbilitySystemComponent.h"
 #include "../TestProject.h"
 #include "Characters/TP_CharacterBase.h"
-
+#include "Components/StaticMeshComponent.h"
 #include "TP_EnemyCharacterBase.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnEnemyDeathDelegate);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInstigateDelegate, UAbilitySystemComponent*, ASCSourceInstigator);
 
 UCLASS(config=Game)
 class TESTPROJECT_API ATP_EnemyCharacterBase : public ATP_CharacterBase
@@ -24,24 +25,52 @@ public:
     // Sets default values for this character's properties
     ATP_EnemyCharacterBase();
 
-    UPROPERTY(BlueprintAssignable, Category = "Abilities | Attributes")
-    FOnEnemyDeathDelegate OnEnemyDeathDelegate;
+	UPROPERTY(BlueprintAssignable, Category = "Abilities | Attributes")
+		FOnEnemyDeathDelegate OnEnemyDeathDelegate;
 
-    UFUNCTION(BlueprintNativeEvent)
+	UPROPERTY(BlueprintAssignable, Category = "Abilities | Attributes")
+		FOnInstigateDelegate OnInstigateDelegate;
+
+    //UFUNCTION(BlueprintNativeEvent)
     void Death();
 
-    virtual void Death_Implementation();
+    //virtual void Death_Implementation();
 
     // Attribute changed callbacks
     virtual void HealthChanged(const FOnAttributeChangeData& Data);
 
 
 protected:
-    // Called when the game starts or when spawned
-    virtual void BeginPlay() override;
 
-    UPROPERTY()
-    bool bIsDead;
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//Source ASC Instigator
+	UAbilitySystemComponent* ASCSourceInstigator = nullptr;
+
+	//Source ASC killer
+	UAbilitySystemComponent* ASCSourceDeath = nullptr;
+
+	FGameplayTag DeadTag;
+
+	//this effects that should instigate enemy 
+	UPROPERTY(EditAnywhere, Category = "EnemyParameters")
+		FGameplayTagContainer InstigateTags;
+
+	/*UPROPERTY(EditAnywhere, Category = "EnemyParameters")
+		int BaseDamage = 0;*/
+
+	UPROPERTY(EditAnywhere, Category = "EnemyParameters")
+		int PointsPerKill=0;
+
+	//UPROPERTY(EditAnywhere, Category = "EnemyParameters")
+	//	float RangeAttack = 0.0f;
+
+	UFUNCTION()
+		void OnGameplayEffectApplied(UAbilitySystemComponent* Source, const FGameplayEffectSpec& Spec, FActiveGameplayEffectHandle Handle);
+
+    //UPROPERTY()
+   // bool bIsDead;
 
 public:
     // Called every frame
@@ -50,4 +79,25 @@ public:
     // Called to bind functionality to input
     virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UFUNCTION(BlueprintCallable, Category = "Abilities | Attributes")
+		bool IsAlive() const;
+
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+		UAbilitySystemComponent* GetASCSourceInstigator();
+
+	//UFUNCTION(BlueprintCallable)
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+		UAbilitySystemComponent* GetASCSourceDeath();
+
+	//void SetSourceAttack(UAbilitySystemComponent* DamageSource);
+	UFUNCTION(BlueprintCallable)
+		void SetASCSourceInstigator(UAbilitySystemComponent* ASCSourceInstigator);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+		int GetPointsPerKill();
+
+	/*UFUNCTION(BlueprintCallable, BlueprintPure)
+		int GetBaseDamage();*/
+	
 };
